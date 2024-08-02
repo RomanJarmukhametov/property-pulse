32f6ee77d0e4ed119bc75847726816ee444de729
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-08-02 15:13:25 +0500

    feat: add global metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import '@/assets/styles/global.css';
 
+export const metadata = {
+  title: 'Property Pulse',
+  description: 'Property Pulse',
+};
+
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html>
